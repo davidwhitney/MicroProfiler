177f2b29ffa8d89f11e760f3bddb3dd6bf180a17
David Whitney <david@davidwhitney.co.uk>
2014-02-03 11:59:16 +0000

    Better xml doc.

--- MicroProfiler/ProfilingDataStorage/IStoreProfilingDataForAUnitOfWork.cs
+++ MicroProfiler/ProfilingDataStorage/IStoreProfilingDataForAUnitOfWork.cs
@@ -5,6 +5,11 @@ namespace MicroProfiler.ProfilingDataStorage
     public interface IStoreProfilingDataForAUnitOfWork
     {
         void Store(ProfiledOperations ops);
+
+        /// <summary>
+        /// Retrieves currently active diagnostics session, returning NULL if no profiling session has been started
+        /// </summary>
+        /// <returns>ProfiledOperations OR null</returns>
         ProfiledOperations Retrieve();
     }
 }
